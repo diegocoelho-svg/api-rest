8267dbbea9fed34e8e1a40de659bf9216f7d3f10
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-05-29 21:45:23 -0300

    Query params

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ const PORT = 3333
 
 const app = express()
 
-app.get('/products/:id/:user', (request, response) => {
-  const { id, user } = request.params
-  response.send(`Produto ${id} do usuário ${user}`)
+app.get('/products', (request, response) => {
+  const { page, limit } = request.query
+  response.send(`Pagina ${page} de ${limit}`)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
