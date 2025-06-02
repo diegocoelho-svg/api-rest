c132acc58d6a920121285874d4c7de56cfcdeb7c
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-06-02 19:19:08 -0300

    Modificando requisição e arquivo d.ts

--- src/middlewares/my-middleware.ts
+++ src/middlewares/my-middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express"
 
 export function myMiddleware(request: Request, response: Response, next: NextFunction) {
-  console.log("Passou pelo Middleware!")
+  request.user_id = "123456"
 
   return next()
 }
